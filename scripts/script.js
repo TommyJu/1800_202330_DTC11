0ad94807834ea822403e43b2776f8b859723f58c
Tommy Ju <tommyju25@gmail.com>
2023-10-29 22:47:54 -0700

    Remove message_board.js and move code to script.js

--- scripts/script.js
+++ scripts/script.js
@@ -10,3 +10,11 @@ function logout() {
         // An error happened.
       });
 }
+
+// scroll to top
+const scrollToTop = document.getElementById("scroll-to-top");
+const cardContainer = document.getElementById("card-container");
+
+scrollToTop.addEventListener("click", () => {
+    cardContainer.scrollTop = 0;
+})
