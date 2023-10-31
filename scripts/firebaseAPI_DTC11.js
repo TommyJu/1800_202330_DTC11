1c48b97d77b5a24478758a97a4edd3ecd6122816
Tommy Ju <tommyju25@gmail.com>
2023-10-30 17:00:21 -0700

    Remove firebase API from .gitignore

---
+++ scripts/firebaseAPI_DTC11.js
@@ -0,0 +1,18 @@
+//----------------------------------------
+//  Your web app's Firebase configuration
+//----------------------------------------
+var firebaseConfig = {
+    apiKey: "AIzaSyAR3eEnEl0KoqReRpsQEhBzig4LFtFC4IE",
+    authDomain: "comp1800-dtc11.firebaseapp.com",
+    projectId: "comp1800-dtc11",
+    storageBucket: "comp1800-dtc11.appspot.com",
+    messagingSenderId: "943216741370",
+    appId: "1:943216741370:web:410909938860a210b67243"
+  };
+
+//--------------------------------------------
+// initialize the Firebase app
+// initialize Firestore database if using it
+//--------------------------------------------
+const app = firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
