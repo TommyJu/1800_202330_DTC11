da8de6cfa40556fd5d9ecb0bed8b77643d537888
Momona Wada <momona.9410@gmail.com>
2023-11-12 14:48:43 -0800

    Display image on view_message.html

--- scripts/view_message.js
+++ scripts/view_message.js
@@ -5,7 +5,7 @@ function dynamicallyPopulatePost(){
 
     db.collection("allPosts").doc(docID)
         .onSnapshot(postID => {
-            console.log(postID.data().category);
+            // console.log(postID.data().category);
             console.log(postID.data().title)
             console.log(postID.data().image)
             console.log(postID.data().description);
@@ -14,6 +14,7 @@ function dynamicallyPopulatePost(){
             document.getElementById("post-title-placeholder").innerText = postID.data().title;
             document.getElementById("post-image-placeholder").src = postID.data().image;
             document.getElementById("post-description-placeholder").innerText = postID.data().description;
+            
         })
 }
 
