c7b5c077d6d5e707aa344f375764c05ed3fe90d2
Tommy Ju <tommyju25@gmail.com>
2023-11-11 19:22:17 -0800

    Create media upload preview

--- scripts/new_post_screen.js
+++ scripts/new_post_screen.js
@@ -45,3 +45,17 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
+var ImageFile;
+function listenFileSelect() {
+      // listen for file selection
+      var fileInput = document.getElementById("media-input"); // pointer #1
+      const image = document.getElementById("media"); // pointer #2
+
+			// When a change happens to the File Chooser Input
+      fileInput.addEventListener('change', function (e) {
+          ImageFile = e.target.files[0];   //Global variable
+          var blob = URL.createObjectURL(ImageFile);
+          image.src = blob; // Display this image
+      })
+}
+listenFileSelect();
