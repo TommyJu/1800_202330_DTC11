a6b4e47960f16bd9754b0343cf4743438c42aeba
Tommy Ju <tommyju25@gmail.com>
2023-11-15 20:40:36 -0800

    Create a new dynamic menu page

--- scripts/menu.js
+++ scripts/menu.js
@@ -16,5 +16,48 @@ function insertNameFromFirestore() {
         }
     })
 }
-
 insertNameFromFirestore(); // Run the function
+
+
+function displayCardsDynamically(collection) {
+    let categoryTemplate = document.getElementById("category-template");
+    let categoryCollection = db.collection(collection);
+
+    categoryCollection.get()
+        .then(allCategories => {
+            // Create each message board post
+            allCategories.forEach(doc => { //iterate thru each doc
+                var title = doc.data().title;
+                var description = doc.data().description;
+                var image = doc.data().image;
+                var docID = doc.id;
+
+                let newcard = categoryTemplate.content.cloneNode(true); // Clone the HTML template to create a new card (newcard) that will be filled with Firestore data.
+
+                // update title, description and image
+                newcard.querySelector('.card-title').innerHTML = title;
+                newcard.querySelector('.card-text').innerHTML = description;
+                newcard.querySelector('.card-img-top').src = image;
+
+                // Add event listener for category button
+                newcard.querySelector(".card-button").addEventListener("click", () => {
+                    storeCategory(title);
+                });
+                console.log(title)
+
+
+                //attach to card-container
+                // document.getElementById("card-container").prepend(newcard);
+                document.getElementById("category-container").append(newcard);
+            })
+        })
+}
+displayCardsDynamically("categories");
+
+
+// Will store the category to localstorage for future use
+function storeCategory(value) {
+    let key = "currentCategory";
+    localStorage.setItem(key, value);
+    console.log("cat clicked!");
+};
